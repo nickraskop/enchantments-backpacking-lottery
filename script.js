0c7b987ac059ceb5316d518b7f54d68a1cbe61eb
nickraskop <raskop@usc.edu>
2021-10-01 18:10:36 -0700

    Created form

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ async function getZones() {
     console.log(zoneFull);
 
 
-    var mainContainer = document.getElementById('myData');
+    var mainContainer = document.getElementById('zone-form');
     for (var i=0; i < 8; i++) {
         var div = document.createElement('div');
         div.innerHTML = 'Zone ' + zoneFull[i][i+1].zone_id + ': ' + zoneFull[i][i+1].name;
@@ -25,5 +25,33 @@ async function getZones() {
 }
 
 
+// var form = document.getElementById('zone-form');
+// form.onsubmit = function(e) {
+//     e.preventDefault();
+//     var result = document.getElementById('result');
+//     result.innerHTML = 'You chose ' + form.zoneChoice.value;
+//     this.reset();
+// }
+
 
 getZones();
+
+// var form = document.getElementById('zone-form').elements;
+// form.elements['zoneChoice'];
+
+// const form = document.querySelector('#zoneChoice');
+// form.addEventListener('submit', function(event) {
+//     event.preventDefault();
+// }
+// console.log(hi);
+
+var form = document.getElementById('zone-form');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault() // Prevent form from autosubmitting
+
+    var zoneChoice = document.getElementById('zonechoice').value;
+    document.getElementById('zone-form').reset();
+    console.log(zoneChoice);
+
+})
