9ed8271ea4244e28bebbaae4022ea9816271588e
nickraskop <raskop@usc.edu>
2021-10-01 19:10:54 -0700

    Prettify

--- script.js
+++ script.js
@@ -1,50 +1,55 @@
-const api_url = 'http://jump.javin.io:5000/api/zones';
+
+// Function to display zones
+const api_zone_url = 'http://jump.javin.io:5000/api/zones';
 async function getZones() {
-    const response = await fetch(api_url);
+    const response = await fetch(api_zone_url);
     const data = await response.json();
-    let zoneFull = data.data;
-    console.log(zoneFull);
+    let zoneData = data.data;
 
-
+    // Loop over each zone in array and append to div
     var mainContainer = document.getElementById('displayZones');
-    for (var i=0; i < 8; i++) {
+    for (var zoneArrayIndex=0; zoneArrayIndex < 8; zoneArrayIndex++) {
         var div = document.createElement('div');
-        div.innerHTML = 'Zone ' + zoneFull[i][i+1].zone_id + ': ' + zoneFull[i][i+1].name;
+        div.innerHTML = 'Zone ' + zoneData[zoneArrayIndex][zoneArrayIndex+1].zone_id + ': ' + zoneData[zoneArrayIndex][zoneArrayIndex+1].name;
         mainContainer.appendChild(div);
     }
-
-    return zoneFull;
- 
-}
-var anotherContainer = document.getElementById('show-award');
-const awards_url = 'http://jump.javin.io:5000/api/awards'
-async function displayAwards(choice) {
-    const response = await fetch(awards_url);
-    const data = await response.json();
-    let awardData = data.data;
-    console.log(awardData);
-    document.getElementById('show-award').innerHTML ="";
-    var awardCounter = 0;
-    for (var j=0; j< 100; j++) {
-        var div = document.createElement('div');
-        if (awardData[j][j+1].zone_id == choice) {
-            awardCounter++;
-            div.innerHTML = 'Award #' + awardData[j][j+1].award_id + ': <br>' +
-            '* Date awarded: ' + awardData[j][j+1].entry + '<br>' +
-            '* Size: ' + awardData[j][j+1].size + '<br>' +
-            '* Pref: ' + awardData[j][j+1].pref;
-            anotherContainer.appendChild(div);
-        }
-    }
-    div.innerHTML = 'There are ' + awardCounter + ' awards for this zone.';
-    anotherContainer.appendChild(div);
 }
 
 getZones();
 
 
+var anotherContainer = document.getElementById('show-award');
+const awards_url = 'http://jump.javin.io:5000/api/awards'
+// Function to display awards
+async function displayAwards(choice) {
+    const response = await fetch(awards_url);
+    const data = await response.json();
+    let awardData = data.data;
+    document.getElementById('show-award').innerHTML ="";
+    var awardCounter = 0;
+    // Loop through every award and check if equal to zone choice
+    for (var awardArrayIndex=0; awardArrayIndex< 100; awardArrayIndex++) {
+        var div = document.createElement('div');
+        if (awardData[awardArrayIndex][awardArrayIndex+1].zone_id == choice) {
+            awardCounter++;
+            div.innerHTML = 'Award #' + awardData[awardArrayIndex][awardArrayIndex+1].award_id + ': <br>' +
+            '* Date awarded: ' + awardData[awardArrayIndex][awardArrayIndex+1].entry + '<br>' +
+            '* Size: ' + awardData[awardArrayIndex][awardArrayIndex+1].size + '<br>' +
+            '* Pref: ' + awardData[awardArrayIndex][awardArrayIndex+1].pref;
+            anotherContainer.appendChild(div);
+        }
+    }
+    // Display sum of awards in this zone
+    div.innerHTML = 'There are ' + awardCounter + ' awards for this zone.';
+    // append div changes to main div
+    anotherContainer.appendChild(div);
+}
+
+
+
 var form = document.getElementById('zone-form');
 
+// Form to choose zone
 form.addEventListener('submit', function(event) {
     event.preventDefault() // Prevent form from autosubmitting
 
