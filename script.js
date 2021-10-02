e1fb303ef9387619563ce8c8365228d2074ec7c5
nickraskop <raskop@usc.edu>
2021-10-01 18:55:32 -0700

    Display more info

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ async function getZones() {
     console.log(zoneFull);
 
 
-    var mainContainer = document.getElementById('zone-form');
+    var mainContainer = document.getElementById('displayZones');
     for (var i=0; i < 8; i++) {
         var div = document.createElement('div');
         div.innerHTML = 'Zone ' + zoneFull[i][i+1].zone_id + ': ' + zoneFull[i][i+1].name;
@@ -16,22 +16,28 @@ async function getZones() {
     return zoneFull;
  
 }
-
+var anotherContainer = document.getElementById('show-award');
 const awards_url = 'http://jump.javin.io:5000/api/awards'
 async function displayAwards(choice) {
     const response = await fetch(awards_url);
     const data = await response.json();
     let awardData = data.data;
     console.log(awardData);
-
-    var anotherContainer = document.getElementById('show-award');
+    document.getElementById('show-award').innerHTML ="";
+    var awardCounter = 0;
     for (var j=0; j< 100; j++) {
+        var div = document.createElement('div');
         if (awardData[j][j+1].zone_id == choice) {
-            var div = document.createElement('div');
-            div.innerHTML = 'Award ' + awardData[j][j+1].entry;
+            awardCounter++;
+            div.innerHTML = 'Award #' + awardData[j][j+1].award_id + ': <br>' +
+            '* Date awarded: ' + awardData[j][j+1].entry + '<br>' +
+            '* Size: ' + awardData[j][j+1].size + '<br>' +
+            '* Pref: ' + awardData[j][j+1].pref;
             anotherContainer.appendChild(div);
         }
     }
+    div.innerHTML = 'There are ' + awardCounter + ' awards for this zone.';
+    anotherContainer.appendChild(div);
 }
 
 getZones();
